c7b7571bec063c993b1caeb1350852ad9de8470f
copag-progep <copag@progep.ufc.br>
2026-03-22 23:18:11 -0300

    chore: publish institutional repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import AppLayout from "./components/AppLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminPage from "./pages/AdminPage";
+import DashboardPage from "./pages/DashboardPage";
+import FlowPage from "./pages/FlowPage";
+import LoginPage from "./pages/LoginPage";
+import LogoutPage from "./pages/LogoutPage";
+import MultiSectorPage from "./pages/MultiSectorPage";
+import ProductivityPage from "./pages/ProductivityPage";
+import StaleProcessesPage from "./pages/StaleProcessesPage";
+import UploadPage from "./pages/UploadPage";
+
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<DashboardPage />} />
+        <Route path="enviar-relatorio" element={<UploadPage />} />
+        <Route path="entradas-saidas" element={<FlowPage />} />
+        <Route path="produtividade" element={<ProductivityPage />} />
+        <Route path="processos-parados" element={<StaleProcessesPage />} />
+        <Route path="multiplos-setores" element={<MultiSectorPage />} />
+        <Route path="administracao" element={<AdminPage />} />
+        <Route path="logout" element={<LogoutPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
